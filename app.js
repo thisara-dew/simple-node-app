5c3d37aeda2425f93cb4c84a694423014f647ae9
thisara-dew <t.dew.senanayake@outlook.com>
2024-03-27 01:33:02 +0530

    Changed get route

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const port = 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 // Route 1: Ger request to '/'
-app.get('/', (req, res) => {
-    const requestData = req.body;
-    // Process requestData...
-    res.send('Received Get request on route 1');
+app.get('/', (request, response) => {
+    return response.send(`
+        <br />
+        <br />
+        <center>
+            <h1>
+                Hello 👋 from Dew
+            </h1>
+        </center>
+    `);
 });
 // Route 1: POST request to '/route1'
 app.post('/route', (req, res) => {
